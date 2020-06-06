2b71463aa9df3c4b175078cf9b10d7591b788cd2
sagudog01 <sagudog01@larioja.edu.es>
2020-06-06 13:31:34 +0200

    Terminado registro, login y consulta básicos

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -13,7 +13,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Entity\UserLog
+use App\Entity\UserLog;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\Config\Definition\Exception\Exception;
@@ -22,6 +22,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
 
@@ -115,8 +117,7 @@ class ApiController extends AbstractFOSRestController
      *
      * @SWG\Tag(name="User")
      */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $encoder) {
-        $serializer = $this->get('jms_serializer');
+    public function registerAction(Request $request, UserPasswordEncoderInterface $encoder, SerializerInterface $serializer) {
         $em = $this->getDoctrine()->getManager();
 
         $user = [];
@@ -137,11 +138,12 @@ class ApiController extends AbstractFOSRestController
             $user->setUsername($username);
             $user->setPlainPassword($password);
             $user->setPassword($encoder->encodePassword($user, $password));
+            $user->setEnabled(true);
 
             $em->persist($user);
             $em->flush();
 
-        } catch (Exception $ex) {
+        } catch (\Exception $ex) {
             $code = 500;
             $error = true;
             $message = "An error has occurred trying to register the user - Error: {$ex->getMessage()}";
@@ -181,8 +183,7 @@ class ApiController extends AbstractFOSRestController
      *
      * @SWG\Tag(name="Log")
      */
-    public function getAllLogsAction(Request $request) {
-        $serializer = $this->get('jms_serializer');
+    public function getAllLogsAction(Request $request, SerializerInterface $serializer) {
         $em = $this->getDoctrine()->getManager();
         $logs = [];
         $message = "";
